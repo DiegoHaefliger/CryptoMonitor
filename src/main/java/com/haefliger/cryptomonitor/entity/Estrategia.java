ee641ca1b0fd11293227b848781e85cff3c935b6
DiegoHaefliger <diegohaefliger@hotmail.com>
2025-06-08 15:41:18 -0300

    CM-1 - ✨ Cria tabelas de estratégias

---
+++ src/main/java/com/haefliger/cryptomonitor/entity/Estrategia.java
@@ -0,0 +1,41 @@
+package com.haefliger.cryptomonitor.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@Getter
+@Setter
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "estrategias")
+public class Estrategia {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 20)
+    private String simbolo;
+
+    @Column(nullable = false, length = 10)
+    private String intervalo;
+
+    @Column(name = "operador_logico", nullable = false, length = 10)
+    private String operadorLogico;
+
+    @Column(name = "date_created", nullable = false, updatable = false, insertable = false)
+    private LocalDateTime dateCreated;
+
+    @OneToMany(mappedBy = "estrategia", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<CondicaoEstrategia> condicoes;
+}
