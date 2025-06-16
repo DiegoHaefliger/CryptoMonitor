8aa8be55d9a49a356e44acbb6768aa7a035cd0fa
DiegoHaefliger <diegohaefliger@hotmail.com>
2025-06-16 19:51:36 -0300

    CM-5 - ✨ Cria agendamento para iniciar o Web Socket

--- src/main/java/com/haefliger/cryptomonitor/CryptoMonitorApplication.java
+++ src/main/java/com/haefliger/cryptomonitor/CryptoMonitorApplication.java
@@ -3,9 +3,11 @@ package com.haefliger.cryptomonitor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableAsync
+@EnableScheduling
 public class CryptoMonitorApplication {
 
     public static void main(String[] args)  {
