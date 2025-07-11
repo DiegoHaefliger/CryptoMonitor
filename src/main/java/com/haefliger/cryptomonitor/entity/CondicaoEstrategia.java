14aa7de99380f85bc67b483f22ad4a6568f0f65c
DiegoHaefliger <diegohaefliger@hotmail.com>
2025-07-11 18:11:31 -0300

    CM-15 - ✨ Refatora classes de entidades
    Signed-off-by: DiegoHaefliger <diegohaefliger@hotmail.com>

--- src/main/java/com/haefliger/cryptomonitor/entity/CondicaoEstrategia.java
+++ src/main/java/com/haefliger/cryptomonitor/entity/CondicaoEstrategia.java
@@ -5,21 +5,19 @@ import com.haefliger.cryptomonitor.enums.OperadorComparacaoEnum;
 import com.haefliger.cryptomonitor.enums.TipoIndicadorEnum;
 import jakarta.persistence.*;
 import lombok.*;
-import org.hibernate.proxy.HibernateProxy;
 
+import java.io.Serializable;
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
-import java.util.Objects;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
+@Data
 @Builder
-@ToString(exclude = {"estrategia"})
+@AllArgsConstructor
+@NoArgsConstructor
 @Entity
+@ToString(exclude = {"estrategia"})
 @Table(name = "condicoes_estrategia")
-public class CondicaoEstrategia {
+public class CondicaoEstrategia implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -49,21 +47,5 @@ public class CondicaoEstrategia {
         if (dateCreated == null) {
             dateCreated = LocalDateTime.now();
         }
-    }
-
-    @Override
-    public final boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null) return false;
-        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
-        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
-        if (thisEffectiveClass != oEffectiveClass) return false;
-        CondicaoEstrategia that = (CondicaoEstrategia) o;
-        return getId() != null && Objects.equals(getId(), that.getId());
-    }
-
-    @Override
-    public final int hashCode() {
-        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
     }
 }
