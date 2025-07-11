9719ef29ad21fa8a34690e3fb71b5a637cc9bfa1
DiegoHaefliger <diegohaefliger@hotmail.com>
2025-07-11 18:19:28 -0300

    CM-15 - ✨ Refatora classes de validator
    Signed-off-by: DiegoHaefliger <diegohaefliger@hotmail.com>

--- src/main/java/com/haefliger/cryptomonitor/validation/validator/IntervaloValidator.java
+++ src/main/java/com/haefliger/cryptomonitor/validation/validator/IntervaloValidator.java
@@ -1,6 +1,7 @@
-package com.haefliger.cryptomonitor.validation;
+package com.haefliger.cryptomonitor.validation.validator;
 
 import com.haefliger.cryptomonitor.enums.IntervaloEnum;
+import com.haefliger.cryptomonitor.validation.IntervaloValido;
 import jakarta.validation.ConstraintValidator;
 import jakarta.validation.ConstraintValidatorContext;
 
